ea515732a762212d5774b094b3d7d38c8279c505
Denoble <ujay2002@yahoo.com>
2018-07-14 19:38:51 -0230

    Update for Samson

---
+++ Appoint/Appoint/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Appoint.Models;
+namespace Appoint.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+        public ViewResult Index() => View(userManager.Users);
+    }
+}
